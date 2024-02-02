392963debf15f92add6e9e224b3f7e75737995c1
thiloschenkelberg <thiloschenkelberg@gmail.com>
2024-02-02 13:59:22 +0100

    Url up

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios"
 import { IOuterDictionary } from "./types/workflow.types"
 
 const USER_API_URL = "https://reactgraphai.ew.r.appspot.com" //google app engine
-const DATA_API_URL =  ""
+const DATA_API_URL =  "http://localhost:8080"
 // const API_URL = "http://localhost:8080" // local
 
 function getCookie(name: string) {
